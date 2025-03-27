ac6d55a7637d170e82a3e10b3f1297d283e008ec
dennisveytsman <dennis.veytsman@gmail.com>
2025-03-27 16:57:01 -0400

    fixed proof of concept

--- src/BirdRecord.java
+++ src/BirdRecord.java
@@ -1,39 +1,96 @@
 import java.util.ArrayList;
 
+import components.sequence.Sequence;
+import components.sequence.Sequence1L;
+
 /**
  * Bird utility class.
  */
 public class BirdRecord {
 
+    /**
+     * Bird record.
+     *
+     * @param species
+     * @param colors
+     * @param location
+     */
+    public record Bird(String species, String color, String location) {
+
+    }
+
+    public Sequence<Bird> birds;
+
+    public BirdRecord() {
+        this.birds = new Sequence1L<>();
+    }
+
+    public void addBird(Bird b) {
+        this.birds.add(0, b);
+    }
+
     //Secondary Methods
     /**
-     * @param birds
      * @param color
      * @return all species of birds of given color
      */
-    public static ArrayList<String> birdsOfAFeather(Bird[] birds,
-            String color) {
+    public ArrayList<String> birdsOfAFeather(String color) {
         ArrayList<String> array = new ArrayList<String>();
-        for (Bird i : birds) {
-            if (i.getColor().equals(color)) {
-                array.add(i.getSpecies());
+        for (Bird i : this.birds) {
+            if (i.color().equals(color)) {
+                array.add(i.species());
             }
         }
         return array;
     }
 
     /**
-     * @param birds
      * @param location
      * @return number of birds in given location
      */
-    public static int birdsInArea(Bird[] birds, String location) {
+    public int birdsInArea(String location) {
         int num = 0;
-        for (Bird i : birds) {
-            if (i.getLocation().equals(location)) {
+        for (Bird i : this.birds) {
+            if (i.location().equals(location)) {
                 num++;
             }
         }
         return num;
+    }
+
+    //Static method
+    /**
+     * @param bird
+     * @return info of bird formatted into sentences.
+     */
+    public static String informationOfBird(Bird bird) {
+        return "This bird is a " + bird.species() + ". It is the color "
+                + bird.color() + ", and is located in " + bird.location() + ".";
+    }
+
+    //Main Method
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        BirdRecord birds = new BirdRecord();
+
+        Bird bird = new Bird("Robin", "Red", "Illinois");
+        System.out.println(bird.color());
+        System.out.println(informationOfBird(bird));
+
+        Bird bird2 = new Bird("Cardinal", "Red", "Ohio");
+
+        birds.addBird(bird2);
+        birds.addBird(bird);
+
+        ArrayList<String> redBirds = new ArrayList<String>();
+        redBirds = birds.birdsOfAFeather("Red");
+        System.out.println("Birds that are red: ");
+        for (String str : redBirds) {
+            System.out.println(str);
+        }
+        System.out.println(
+                "Number of birds in Ohio: " + birds.birdsInArea("Ohio"));
     }
 }
